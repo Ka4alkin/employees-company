b475ae62ce26f6784a7c51a070e7a4739cd60238
Nazar <doskoch25@gmail.com>
2021-12-30 03:57:03 +0200

    Controlled and uncontrolled form inputs

--- my-app/src/components/employers-add-form/employers-add-form.js
+++ my-app/src/components/employers-add-form/employers-add-form.js
@@ -1,24 +1,59 @@
 import './employees-add-form.css'
+import {Component} from "react";
 
-const EmployersAddForm = () => {
-    return (
-        <div className="app-add-form">
-            <h3>Добавьте нового сотрудника</h3>
-            <form
-                className="add-form d-flex">
-                <input type="text"
-                       className="form-control new-post-label"
-                       placeholder="Как его зовут?"/>
-                <input type="number"
-                       className="form-control new-post-label"
-                       placeholder="З/П в $?"/>
+class EmployersAddForm extends Component {
 
-                <button type="submit"
-                        className="btn btn-outline-light">Добавить
-                </button>
-            </form>
-        </div>
-    )
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            salary: ''
+        }
+
+    }
+
+    onValueFormChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+            [e.target.salary]: e.target.salary,
+        })
+    }
+
+
+    render() {
+
+        const {name,salary} = this.state
+
+        return (
+            <div className="app-add-form">
+                <h3>Добавьте нового сотрудника</h3>
+                <form
+                    className="add-form d-flex">
+                    {/*управляемий елемент
+                    двойное связивание
+                    исключение: инпут тип ФАЙЛ
+                    */}
+                    <input type="text"
+                           onChange={this.onValueFormChange}
+                           name="name"
+                           value={name}
+                           className="form-control new-post-label"
+                           placeholder="Как его зовут?"/>
+                    <input type="number"
+                           onChange={this.onValueFormChange}
+                           name="salary"
+                           value={salary}
+                           className="form-control new-post-label"
+                           placeholder="З/П в $?"/>
+
+                    <button type="submit"
+                            className="btn btn-outline-light">Добавить
+                    </button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default EmployersAddForm;
